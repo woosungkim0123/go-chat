41b8ca1ea8a26c78688df896080be74f80532b03
Woosung_Kim <sibu2005@naver.com>
2024-03-28 21:26:01 +0900

    feat: 에러 정의

---
+++ internal/apperrors/custom_error.go
@@ -0,0 +1,6 @@
+package apperrors
+
+type CustomError struct {
+	Code    string
+	Message string
+}
---
+++ internal/apperrors/error_code.go
@@ -0,0 +1,5 @@
+package apperrors
+
+const (
+	NotFoundUserNameError = `NotFoundUserNameError`
+)
